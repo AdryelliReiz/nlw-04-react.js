a4259a1ed0335c3ed4c94faa35bcece1fb700185
Adryelli <adryellireis0santos@gmail.com>
2021-08-16 19:32:12 -0300

    User manipulation features

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 interface UserData {
@@ -10,16 +12,17 @@ interface IProfileProps {
     user: UserData
 }
 
-export function Profile({user}: IProfileProps) {
+export function Profile({ user }: IProfileProps) {
+    const { level } = useContext(ChallengesContext);
 
     return(
         <div className={styles.profileContainer}>
-            <img src="https://github.com/adryellireiz.png" alt={user.username} />
+            <img src="icons/profile.svg" alt={user.username} />
             <div>
                 <strong>{user.username}</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                        Level {user.level}
+                        Level {level}
                 </p>
             </div>
         </div>
--- src/pages/ranking/index.tsx
+++ src/pages/ranking/index.tsx
@@ -1,5 +1,7 @@
+import Cookies from "js-cookie";
 import Head from "next/head";
-import { useContext } from "react";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 import { ThemeProvider } from "styled-components"
 import { HeaderRanking } from "../../components/HeaderRanking";
 import { RankingContent} from "../../components/RankingContent";
@@ -8,7 +10,21 @@ import { ThemeContextLD } from "../../contexts/ThemeContext";
 import GlobalStyled from '../../styles/global';
 
 export default function Ranking() {
-    const {theme} = useContext(ThemeContextLD);
+    const { theme } = useContext(ThemeContextLD);
+    const router = useRouter();
+    
+    useEffect(() => {
+        const cookieToken = Cookies.get('token');
+        try {
+            if (!cookieToken || cookieToken === '') {
+                router.push('/login')
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }, []);
+    
     return(
         <ThemeProvider theme={theme}>
             <GlobalStyled/>
